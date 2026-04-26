958bb83970b53fd8ce5d2b1256e7c4aeb3bddf71
Zhang Huanjie <james@ustc.edu.cn>
2026-04-26 08:11:43 +0800

    更新版本号为 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.example.wifibssquery"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
